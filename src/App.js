0e3a7deb95e2b595b3c30d167c947384823cf65c
Faraz <faraziqbal@hotmail.co.uk>
2021-04-19 16:48:03 +0530

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+/* eslint-disable react/jsx-no-target-blank */
 import "./App.css";
 import Home from "./screens/Home";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import Region from "./screens/Region";
+import { Card } from "react-bootstrap";
 
 function App() {
   return (
@@ -10,6 +12,37 @@ function App() {
         <Route path="/:id" component={Region} />
         <Route exact path="/" component={Home} />
       </Router>
+      <Card className="mt-4">
+        <Card.Body>
+          <blockquote className="blockquote mb-0 text-center">
+            <p>
+              {" "}
+              In case you want to lend a helping hand or have verified resouces,
+              you can reach out to me via{" "}
+              <div className="foot">
+                <li>
+                  <a
+                    className="foot"
+                    href="mailto:faraziqbal2001@gmail.com"
+                    target="_blank"
+                  >
+                    Email
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="foot"
+                    href="https://instagram.com/faraziqbaal"
+                    target="_blank"
+                  >
+                    Instagram
+                  </a>
+                </li>
+              </div>
+            </p>
+          </blockquote>
+        </Card.Body>
+      </Card>
     </>
   );
 }
